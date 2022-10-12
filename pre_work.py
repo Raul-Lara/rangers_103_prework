f46f12cfadd674a4b5567ea8b72ef48c6d394dfe
Raul-Lara <100654697+Raul-Lara@users.noreply.github.com>
2022-10-11 22:13:46 -0400

    Update pre_work.py

--- pre_work.py
+++ pre_work.py
@@ -1,4 +1,40 @@
+#########################
+###### Raul Lara ########
+#########################
 
 
+#question 1
+def hello_name(user_name):
+  user_name = input("Enter USERNAME")
+  print('hello_', user_name)
 
-#print all odd numbers between 1 and 100
+#question 2
+def first_odds():
+  for i in range (0, 101):
+    if (i % 2 != 0 ):
+      print(i)
+      
+#question 3
+def max_num_in_list(a_list):
+  maxValue = 0
+  size = len(a_list)
+  for i in range(size):
+    if(a_list[i] > maxValue):
+      maxValue = a_list[i]
+
+#question 4
+def is_leap_year(a_year):
+  if(a_year % 4 == 0) & (a_year % 100 != 0) | (a_year % 400 == 0):
+    return True
+  else:
+    return False
+
+#question 5
+def is_consecutive(a_list):
+  difference = 1
+  size = len(a_list)
+  for i in range(size):
+    if(a_list[i] - a_list[i-1] != 1):
+      return False
+    else:
+      return True
